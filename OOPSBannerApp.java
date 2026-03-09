fe8ed758ff6b22f6aa4cc8cbc9443a678aa413fe
shruti071206 <sp6923@srmist.edu.in>
2026-03-10 01:09:41 +0530

    UC6 OOPS Banner using static helper methods

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -3,17 +3,53 @@ public class OOPSBannerApp {
     public static void main(String[] args) {
 
         String[] banner = {
-            String.join("   ", "*****", "*****", "*****", "*****"),
-            String.join("   ", "*   *", "*   *", "*   *", "*   *"),
-            String.join("   ", "*   *", "*   *", "*   *", "*   *"),
-            String.join("   ", "*   *", "*   *", "*   *", "*****"),
-            String.join("   ", "*   *", "*   *", "*   *", "*"),
-            String.join("   ", "*   *", "*   *", "*   *", "*"),
-            String.join("   ", "*****", "*****", "*****", "*****")
+            String.join("   ", getO()[0], getO()[0], getP()[0], getS()[0]),
+            String.join("   ", getO()[1], getO()[1], getP()[1], getS()[1]),
+            String.join("   ", getO()[2], getO()[2], getP()[2], getS()[2]),
+            String.join("   ", getO()[3], getO()[3], getP()[3], getS()[3]),
+            String.join("   ", getO()[4], getO()[4], getP()[4], getS()[4]),
+            String.join("   ", getO()[5], getO()[5], getP()[5], getS()[5]),
+            String.join("   ", getO()[6], getO()[6], getP()[6], getS()[6])
         };
 
         for (String line : banner) {
             System.out.println(line);
         }
+    }
+
+    static String[] getO() {
+        return new String[]{
+                "*****",
+                "*   *",
+                "*   *",
+                "*   *",
+                "*   *",
+                "*   *",
+                "*****"
+        };
+    }
+
+    static String[] getP() {
+        return new String[]{
+                "*****",
+                "*   *",
+                "*   *",
+                "*****",
+                "*",
+                "*",
+                "*"
+        };
+    }
+
+    static String[] getS() {
+        return new String[]{
+                "*****",
+                "*",
+                "*",
+                "*****",
+                "    *",
+                "    *",
+                "*****"
+        };
     }
 }
